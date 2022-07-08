93a963aa9dcf4589a756c256fadcb0a56c71f04e
terminator090 <66550325+mohdsaif0@users.noreply.github.com>
2022-07-08 18:36:22 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -128,8 +128,8 @@ def get_progress_bar_string(status):
     p = 0 if total == 0 else round(completed * 100 / total)
     p = min(max(p, 0), 100)
     cFull = p // 7
-    p_str = '♛' * cFull
-    p_str += '♕' * (14 - cFull)
+    p_str = '♣' * cFull
+    p_str += '♧' * (14 - cFull)
     p_str = f"├「{p_str}」"
     return p_str
 
